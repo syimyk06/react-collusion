a2d32251cab22d1bc80531ec38a17708d1726419
SyimykRavshanbekov <programmist00707@gmail.com>
2024-05-17 08:36:43 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.css";
-import { FASHION_WOMENS_INITIAL_DATA, FASHION_MENS_INITIAL_DATA } from "./Data.js";
+import {
+  FASHION_WOMENS_INITIAL_DATA,
+  FASHION_MENS_INITIAL_DATA,
+} from "./Data.js";
 import { Fashion } from "./Components/Fashion/Fashion.jsx";
 import { Header } from "./Components/Header/Header.jsx";
 import { CardList } from "./Components/CardList/CardList.jsx";
@@ -11,18 +14,21 @@ export const App = () => {
     <>
       <Header />
       <Routes>
+        <Hero />
+        <Fashion
+          title={"WOMEN'S FASHION"}
+          subTitle={"Shop our new arrivals from established brands"}
+        >
+          <CardList items={FASHION_WOMENS_INITIAL_DATA} />
+        </Fashion>
 
-      
-      <Hero />
-      <Fashion title={"WOMEN'S FASHION"} subTitle={"Shop our new arrivals from established brands"}>
-        <CardList items={FASHION_WOMENS_INITIAL_DATA} />
-      </Fashion>
-
-      <Fashion title={"MEN'S FASHION"} subTitle={"Shop our new arrivals from established brands"}>
-        <CardList items={FASHION_MENS_INITIAL_DATA} />
-      </Fashion>
+        <Fashion
+          title={"MEN'S FASHION"}
+          subTitle={"Shop our new arrivals from established brands"}
+        >
+          <CardList items={FASHION_MENS_INITIAL_DATA} />
+        </Fashion>
       </Routes>
     </>
   );
-  
 };
