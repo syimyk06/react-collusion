fed34ccdb1bbad02bb9379ca7338544f79641623
SyimykRavshanbekov <programmist00707@gmail.com>
2024-05-17 09:44:47 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,21 @@ import { Fashion } from "./Components/Fashion/Fashion.jsx";
 import { Header } from "./Components/Header/Header.jsx";
 import { CardList } from "./Components/CardList/CardList.jsx";
 import { Hero } from "./Components/Hero/Hero.jsx";
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export const App = () => {
   return (
     <>
       <Header />
       <Routes>
-        <Hero />
-        <Fashion
+        <Route path="/" element={<Hero />}/>
+        
+      </Routes>
+    </>
+  );
+};
+
+{/* <Fashion
           title={"WOMEN'S FASHION"}
           subTitle={"Shop our new arrivals from established brands"}
         >
@@ -27,8 +33,4 @@ export const App = () => {
           subTitle={"Shop our new arrivals from established brands"}
         >
           <CardList items={FASHION_MENS_INITIAL_DATA} />
-        </Fashion>
-      </Routes>
-    </>
-  );
-};
+        </Fashion> */}
